e3f0c58a2df21184706924ee263e478dc503b9a9
brownyin365 <brownyin365@github.com>
2018-10-27 03:54:57 -0700

    add exiting file

--- DataHouse/settings.py
+++ DataHouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,3 +173,4 @@ DEFAULT_FROM_EMAIL = '365serversgh@gmail.com'
 
 
 
+django_heroku.settings(locals())
